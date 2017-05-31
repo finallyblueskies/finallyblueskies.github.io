c221b12d1d9f8dad4403ceb27ce51a5f9c6c4ebb
finallyblueskies <screeds-abuzz-08@icloud.com>
2017-05-31 21:12:54 +0100

    Settling on hover effects for main navigation

--- src/core/main.js
+++ src/core/main.js
@@ -4,29 +4,55 @@ import Nav from 'nav';
 class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      hoverEffectStyles: {},
+      hoverEffectClass: ''
+    };
+
+    this.updateNavHover = this.updateNavHover.bind(this);
   }
 
   updateNavHover(e, mouseOnElement) {
     if (mouseOnElement) {
       const el = e.currentTarget;
+      const hel = document.querySelector('.hover-effect');
       // Work out position and size of effect
       // -- find x offset of hovered icon from start of container
-      // -- add icon width/2
+      const left = el.offsetLeft + el.offsetWidth / 2 - hel.offsetWidth / 2;
       // -- set hover effect width
-      // -- set hover effect x position
+      // -- add icon width/2
+      const hoverEffectClass = `style-${el.getAttribute('class')}`;
       // -- set hover effect styles
-      // -- if hover effect was visible, set travelling transition
+      this.setState({
+        hoverEffectClass,
+        hoverEffectStyles: {
+          opacity: '1',
+          left
+        }
+      });
     } else {
       // Set hover effect style width: 0
-      // on animation finish
+      this.setState({
+        hoverEffectStyles: {
+          ...this.state.hoverEffectStyles,
+          opacity: '0'
+        }
+      });
     }
   }
 
   render() {
     const { updateNavHover } = this;
+    const { hoverEffectStyles, hoverEffectClass } = this.state;
     return (
       <div>
-        <Nav {...{ updateNavHover }} />
+        <Nav
+          {...{
+            updateNavHover,
+            hoverEffectStyles,
+            hoverEffectClass
+          }}
+        />
         {this.props.children}
         {/*<Footer />*/}
       </div>
--- src/nav/index.js
+++ src/nav/index.js
@@ -2,44 +2,59 @@ import 'nav/style.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default ({ updateNavHover }) => {
+export default ({ updateNavHover, hoverEffectStyles, hoverEffectClass }) => {
   const linkHoverIn = {
-    onMouseEnter: e => updateNavHover(e, true)
-  };
-  const navHoverOut = {
+    onMouseEnter: e => updateNavHover(e, true),
     onMouseLeave: e => updateNavHover(e, false)
   };
+  const navHoverOut = {};
   return (
     <nav>
-      <h1>Bogdan's Website</h1>
+      <h1>Bogdan Protsenko</h1>
+      <h2>Front end developer based in London.</h2>
       <hr />
-      <div className="links">
-        <a className="github" href="www.github.com" {...hoverEvents}>
-          <span className="icon" />
-          <span className="icon-decoration" />
-          <span className="label">GitHub</span>
+      <div className="links" {...navHoverOut}>
+        <div
+          className={`hover-effect ${hoverEffectClass}`}
+          style={hoverEffectStyles}
+        >
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+        <a className="github" href="www.github.com" {...linkHoverIn}>
+          <div className="inner">
+            <span className="icon" />
+            <span className="icon-decoration" />
+            <span className="label">GitHub</span>
+          </div>
         </a>
-        <Link className="projects" to="projects" {...hoverEvents}>
-          <span className="icon" />
-          <div className="icon-decoration layer-1">
-            <div className="square-2" />
-            <div className="square-3" />
-            <div className="square-4" />
-            <div className="square-5" />
+        <Link className="projects" to="projects" {...linkHoverIn}>
+          <div className="inner">
+            <span className="icon" />
+            <div className="icon-decoration layer-1">
+              <div className="square-2" />
+              <div className="square-3" />
+              <div className="square-4" />
+              <div className="square-5" />
+            </div>
+            <div className="icon-decoration layer-2" />
+            <div className="icon-decoration layer-3">
+              <div className="square-1" />
+            </div>
+            <span className="label">Projects</span>
           </div>
-          <div className="icon-decoration layer-2" />
-          <div className="icon-decoration layer-3">
-            <div className="square-1" />
-          </div>
-          <span className="label">Projects</span>
         </Link>
-        <Link className="contact" to="contact" {...hoverEvents}>
-          <span className="icon" />
-          <div className="icon-decoration layer-1" />
-          <div className="icon-decoration layer-2">
-            <div className="square-1" />
+        <Link className="contact" to="contact" {...linkHoverIn}>
+          <div className="inner">
+            <span className="icon" />
+            <div className="icon-decoration layer-1" />
+            <div className="icon-decoration layer-2">
+              <div className="square-1" />
+            </div>
+            <span className="label">Contact</span>
           </div>
-          <span className="label">Contact</span>
         </Link>
       </div>
     </nav>
