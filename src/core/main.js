e06e931f4411f941a7ddaa92f53aa53b5e6108de
Bogdan Protsenko <bogdanvp@gmail.com>
2017-06-02 08:15:01 +0100

    Routing and navigation
    - Temporarily route all requests through index until server side rendering is ready
    - Set up projects page
    - Made nav responsive

--- src/core/main.js
+++ src/core/main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Nav from 'nav';
+import { Route } from 'react-router-dom';
+import Projects from 'projects';
+
+// import Contact from 'core/contact';
 
 class Main extends React.Component {
   constructor(props) {
@@ -7,26 +11,48 @@ class Main extends React.Component {
     this.state = {
       hoverEffectStyles: {},
       hoverEffectClass: '',
+      navMinusTop: 0,
       viewClass: ''
     };
 
     this.updateNavHover = this.updateNavHover.bind(this);
   }
 
+  // 1. Set correct view class
+  // 2. Set correct minus top amount
+  // 3. Attach global onResize listener
   componentDidMount() {
     this.setViewClass(this.props);
+    this.navSpacing();
+    window.addEventListener('resize', () => this.navSpacing());
   }
 
   componentWillReceiveProps(nextProps) {
     this.setViewClass(nextProps);
   }
 
+  // Lets the app know whether to display
+  // the intro view or the page view
   setViewClass({ location: { pathname } }) {
     this.setState({
       viewClass: pathname !== '/' ? 'page-view' : ''
     });
   }
 
+  // Gets the px. value to correctly offset the nav
+  // in the Y position so it sticks to the top,
+  // hiding the space used by the title
+  navSpacing() {
+    const els = ['nav h1', 'nav h2', 'nav .spacing'];
+    this.setState({
+      navMinusTop: els
+        .map(selector => document.querySelector(selector).offsetHeight)
+        .reduce((a, b) => a + b, 0)
+    });
+  }
+
+  // Updates the position and individual styles
+  // of the navigation hover effect
   updateNavHover(e, mouseOnElement) {
     if (mouseOnElement) {
       const el = e.currentTarget;
@@ -58,17 +84,25 @@ class Main extends React.Component {
 
   render() {
     const { updateNavHover } = this;
-    const { hoverEffectStyles, hoverEffectClass, viewClass } = this.state;
+    const {
+      hoverEffectStyles,
+      hoverEffectClass,
+      viewClass,
+      navMinusTop,
+      navLinksHeight
+    } = this.state;
     return (
       <div className={viewClass}>
         <Nav
           {...{
             updateNavHover,
             hoverEffectStyles,
-            hoverEffectClass
+            hoverEffectClass,
+            ...(viewClass && { navMinusTop })
           }}
         />
-        {this.props.children}
+        {/*Properly position the page*/}
+        <Route path="/projects" component={Projects} />
         {/*<Footer />*/}
       </div>
     );
