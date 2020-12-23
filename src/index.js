64b6f3d2ee7b74f25db0f8ec265e6fe45bb1bfb3
Bogdan Protsenko <bogdan@grabyo.com>
2020-12-23 12:39:30 +0000

    🔥

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './main/component';
-import "@fortawesome/fontawesome-free/css/all.css"
-import "bulma/css/bulma.css";
-import "./index.css";
-
-ReactDOM.render(<Main />, document.getElementById('root'));
